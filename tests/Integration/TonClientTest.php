8ee390438500d1a9435a7f87967287870f4aacc2
Maxim Karanaev <imaxvx@gmail.com>
2021-05-31 22:14:00 +0300

    Update ton sdk library to 1.16.0

--- tests/Integration/TonClientTest.php
+++ tests/Integration/TonClientTest.php
@@ -22,7 +22,7 @@ class TonClientTest extends AbstractModuleTest
         $expected = new ResultOfVersion(
             new Response(
                 [
-                    'version' => '1.15.0'
+                    'version' => '1.16.0'
                 ]
             )
         );
@@ -48,7 +48,7 @@ class TonClientTest extends AbstractModuleTest
     {
         $resultOfGetApiReference = $this->tonClient->getApiReference();
 
-        self::assertEquals('1.15.0', $resultOfGetApiReference->getApi()['version']);
+        self::assertEquals('1.16.0', $resultOfGetApiReference->getApi()['version']);
         self::assertCount(9, $resultOfGetApiReference->getApi()['modules']);
     }
 }
